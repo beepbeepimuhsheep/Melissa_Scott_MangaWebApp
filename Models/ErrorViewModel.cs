c6cca342a83b728664f2f70bad8a78373dcb1fa5
9189Labs <9189@ctucareer.co.za>
2022-06-29 14:00:46 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Melissa_Scott_9189_FA2_IPG521.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
